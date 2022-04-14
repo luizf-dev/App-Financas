fb991ef7d05582ee8cfa5434dc105a5ba84b99bb
Luiz Fernando <luizfvidal13@hotmail.com>
2022-04-13 22:33:53 -0300

    add formulario de registros

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,3 +1,4 @@
+//EXIBE MENU PARA DISPOSITIVOS MÓVEIS AO CLICK DO MOUSE
 function showMenu(){
     let menuMobile = document.querySelector('.mobile-menu');
     let bgMenu = document.querySelector('.header');
@@ -5,10 +6,22 @@ function showMenu(){
     if(menuMobile.classList.contains('open')){
         menuMobile.classList.remove('open');
         bgMenu.classList.remove('open');
-        document.querySelector('.icon').src = "assets/img/bars-solid.svg";
+        //document.querySelector('.icon').src = "assets/img/bars-solid.svg";
     }else{
         menuMobile.classList.add('open');
         bgMenu.classList.add('open');
-        document.querySelector('.icon').src = "assets/img/window-close-solid.svg";
+       //document.querySelector('.icon').src = "assets/img/window-close-solid.svg";
     }
 }
+
+//ALTERA BACKGROUND DO MENU AO SCROLL DA PAGINA
+  $(function(){   
+    var nav = $('#header');   
+    $(window).scroll(function () { 
+        if ($(this).scrollTop() > 1) { 
+            nav.addClass("menu-fixo");            
+        } else { 
+            nav.removeClass("menu-fixo");             
+        } 
+    });  
+});
