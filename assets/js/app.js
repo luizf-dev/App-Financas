4dd2457b96c765e3ef1c22ccfccd3c221ec08dc4
Luiz Fernando <luizfvidal13@hotmail.com>
2022-06-05 09:37:02 -0300

    criado a tabela dinamica dos registros

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,8 @@
-
-
 //===============LIMPAR OS CAMPOS DOs INPUTS
 const clearFields = () => {
     const fields = document.querySelectorAll('.input-field');
     fields.forEach(field => field.value = "");
 }//============================
-
-
-
 
 
 //=================CLASSES=============================//
@@ -57,6 +52,32 @@ class DB {
         localStorage.setItem('id', id);
     }
     
+    recuperarTodosRegistros(){
+
+        //Array de despesas
+        let despesas = [];
+
+        let id = localStorage.getItem('id');
+
+        //recupera todas as despesas cadastradas em localStorage
+        for(let i = 1; i <= id; i ++){
+             //recupera a despesa
+            let despesa = JSON.parse(localStorage.getItem(i));
+
+        //verifica se índices foram removidos do localStorage e que se tornaram nulos
+        //se houver, nesse caso o índice será pulado 
+
+        if(despesa === null){
+
+            continue
+        }
+
+            despesas.push(despesa);
+        }     
+        
+        return despesas;
+        
+    }
 }
 let db = new DB();
 
@@ -92,6 +113,44 @@ function addDespesa(){
 }
 
 //============LISTA AS DESPESAS========================//
+function carregarListaDespesas(){
+
+    let despesas = [];
+    despesas = db.recuperarTodosRegistros();
+
+    //seleciona o elemento tbody da tabela
+    var listaDespesas = document.getElementById('myTable')
+    /*
+    <tr>
+        <td data-label="Ano"></td>
+        <td data-label="Mês"></td>
+        <td data-label="Dia"></td>
+        <td data-label="Categoria"></td>
+        <td data-label="Descrição"></td>
+        <td data-label="Valor"></td>
+    </tr>
+    */
+
+    //percorre o array despesas, listando de forma dinâmica todos os registros
+    despesas.forEach(function(d){
+
+        //criando a linha -> 'tr'// 
+        let linha = listaDespesas.insertRow();
+
+        //criando as colunas -> 'td'// 
+        linha.innerHTML = 
+        `
+            <td data-label="Data">${d.dia}/${d.mes}/${d.ano}</td>
+            <td data-label="Categoria">${d.tipo}</td>
+            <td data-label="Descrição">${d.descricao}</td>
+            <td data-label="Valor">${d.valor}</td>
+            <td data-label="Ações">
+                <button type="button" class="btn-edit">Edd</button>
+                <button type="button" class="btn-delete">Del</button>
+            </td>
+        `    
+    })
+}
 
 
 
